6132063f42c3923a5b507380c13baad0585890e5
Gökçe <gokce@kreatif-isler.com>
2021-08-17 01:05:38 +0300

    first commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/gokceturkay/Atolyeler/proje_me-ybs_2021/me-ybs_v1/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
