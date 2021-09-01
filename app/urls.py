fec03c444e2a4d5d180707dded5f20927bb8e248
Gökçe <gokce@kreatif-isler.com>
2021-09-01 11:11:07 +0300

    template a içerik çekme

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+    path('students_list.html', views.students_list, name='students_list'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from django.utils import timezone
 from .models import Student, Instructor, Lessons, Custodian
 
 def dashboard(request):
     return render(request, 'index.html', {})
+
+def students_list(request):
+    list_students = Student.objects.filter(created__lte=timezone.now()).order_by('created')
+    return render(request, 'students_list.html', {'list_students':list_students})
 
 
 
