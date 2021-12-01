087296b7794b6af60d08e60e25d5fee029abfcda
Gökçe <gokce@kreatif-isler.com>
2021-12-01 16:45:09 +0300

    Login senaryosu part 1

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from django.urls import path, include
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+    path('hesap/', include('django.contrib.auth.urls')),
+    path('panel', views.panel, name='panel'),
     path('students_list', views.students_list, name='students_list'),
     path('student/edit', views.post_new, name='post_new'),
     path('student_panel', views.student_panel, name='student_panel'),
--- app/views.py
+++ app/views.py
@@ -5,6 +5,10 @@ from django.utils import timezone
 from .forms import StudentForm
 from django.shortcuts import redirect
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+from django.contrib.auth import get_user_model
+
 
 def dashboard(request):
     return render(request, 'index.html', {})
@@ -13,10 +17,14 @@ def students_list(request):
     list_students = Student.objects.filter(created__lte=timezone.now()).order_by("created")
     return render(request,'students_list.html', {'list_students':list_students})
 
-def student_panel(request):
-    students_name = Student.full_name
-    return render(request, 'spanel.html', {"students_name":students_name})
+#### Burada uğraşıyoruz. Giriş yapanın adını çekmek istiyoruz
 
+def student_panel(request):
+    if request.user.is_authenticated:
+        students_name = request.user.get_full_name()
+        return render(request, 'spanel.html', {"students_name":students_name})
+    else:
+        return render(request, 'index.html', {})
 def post_new(request):
 
     if request.method == "POST":
@@ -38,6 +46,28 @@ def post_new(request):
         form = StudentForm()
     return render(request, 'student_edit.html', {'form': form})
 
+#def login_view(request):
+#    if request.method == 'POST':
+#       username = request.POST.get('username')
+#        password = request.POST.get('password')
+#        user = authenticate(username=username, password=password)
+#        if user:
+#            if user.is_active:
+#                login(request, user)
+#                return HttpResponseRedirect('/panel/')
+#            else:
+#                return HttpResponse("Hesabını actif değil")
+#        else:
+#            print("Hatalı giriş bilgileri: {0}, {1}").format(username, password)
+#            return HttpResponse("Hatalı giriş bilgileri alındı.")
+#    else:
+#        return render(request, 'registration/login.html', {}) 
 
 
-    
+#çalışılıyor
+def panel(request):
+    umodel = get_user_model()
+    if umodel == "Student":
+        return HttpResponse("Öğrenci")
+    else:
+        return HttpResponse("Başka")
