300a5850a97ded50781c06ec6cac5ec300e99937
Gökçe <gokce@kreatif-isler.com>
2021-12-01 17:16:47 +0300

    öğretmen mesaj göndermek istiyor. ders seçmeli

--- app/forms.py
+++ app/forms.py
@@ -8,3 +8,10 @@ class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
         fields = ("tc","first_name" ,"last_name","birthday","address","school","phone_no","email")
+        
+class InfoForm(forms.ModelForm):
+    title = forms.CharField(max_length=100)
+    #lesson = 
+    message = forms.CharField(widget=forms.Textarea)
+    
+    
